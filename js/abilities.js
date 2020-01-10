5d90133e3927d433c1b5a27315adc4048fd99227
Ryin Smith <ryin.smith@gmail.com>
2020-01-10 00:44:20 -0600

    Began entering class abilities into objects

---
+++ js/abilities.js
@@ -0,0 +1,42 @@
+const ABILITY_DATA = [
+	{
+		"name" : "Sprint",
+		"level" : ["Base", 1],
+		"desc" : "Increases your movement speed by 30% of your base movement while your endurance lasts. Drains 5 endurance per second while active."
+	},
+	{
+		"name" : "Quick Cast",
+		"level" : ["Base", 1],
+		"desc" : "Your next spell will not be interrupted. Your cast time will not be modified by any bonus including dexterity. Can be reused after 30 seconds."
+	}, 
+	{
+		"name" : "Evade I",
+		"level" : ["Base", 1],
+		"desc" : "Increases your chance to evade by 5%."
+	},
+	{
+		"name" : "Tireless",
+		"level" : ["Base", 1],
+		"desc" : "Increases endurance regeneration. Does not stack with other endurance regeneration spells or effects."
+	}
+];
+
+const WEAPONRY_DATA = [
+	{
+		"name" : "Polearms",
+		"dexc" : "Grants the ability to use polearms."
+	},
+	{
+		"name" : "Staves",
+		"desc" : "Grants the ability to use staves."
+	}
+];
+
+const ARMOR_ABILITY_DATA = [
+	
+];
+
+const SHIELD_ABILITY_DATA = [
+	
+];
+
