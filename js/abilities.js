157cc2ca0aa14b346a1acbfafca1169695351b7e
Ryin Smith <ryin.smith@gmail.com>
2020-01-16 14:15:32 -0600

    Updated ability objects format and added more

--- js/abilities.js
+++ js/abilities.js
@@ -1,42 +1,136 @@
 const ABILITY_DATA = [
 	{
 		"name" : "Sprint",
-		"level" : ["Base", 1],
 		"desc" : "Increases your movement speed by 30% of your base movement while your endurance lasts. Drains 5 endurance per second while active."
 	},
 	{
 		"name" : "Quick Cast",
-		"level" : ["Base", 1],
 		"desc" : "Your next spell will not be interrupted. Your cast time will not be modified by any bonus including dexterity. Can be reused after 30 seconds."
 	}, 
 	{
 		"name" : "Evade I",
-		"level" : ["Base", 1],
 		"desc" : "Increases your chance to evade by 5%."
 	},
 	{
+		"name" : "Evade II",
+		"desc" : "Increases your chance to evade by 10%."
+	},
+	{
 		"name" : "Tireless",
-		"level" : ["Base", 1],
 		"desc" : "Increases endurance regeneration. Does not stack with other endurance regeneration spells or effects."
 	}
 ];
 
 const WEAPONRY_DATA = [
 	{
-		"name" : "Polearms",
-		"dexc" : "Grants the ability to use polearms."
-	},
-	{
 		"name" : "Staves",
 		"desc" : "Grants the ability to use staves."
+	},
+	{
+		"name" : "Polearms",
+		"desc" : "Grants the ability to use polearms."
+	},
+	{
+		"name" : "Two-Handed",
+		"desc" : "Grants the ability to use two handed weapons."
+	},
+	{
+		"name" : "Slashing",
+		"desc" : "Grants the ability to use slashing weapons."
+	},
+	{
+		"name" : "Crushing",
+		"desc" : "Grants the ability to use crushing weapons."
+	},
+	{
+		"name" : "Thrusting",
+		"desc" : "Grants the ability to use thrusting weapons."
+	},
+	{
+		"name" : "Flexible",
+		"desc" : "Grants the ability to use flexible weapons."
+	},
+	{
+		"name" : "Crossbows",
+		"desc" : "Grants the ability to use crossbows."
+	},
+	{
+		"name" : "Shortbows",
+		"desc" : "Grants the ability to use shortbows."
+	},
+	{
+		"name" : "Archery",
+		"desc" : "Grants the ability to use longbows."
+	},
+	{
+		"name" : "Blades",
+		"desc" : "Grants the ability to use blades."
+	},
+	{
+		"name" : "Blunt",
+		"desc" : "Grants the ability to use blunt weapons."
+	},
+	{
+		"name" : "Piercing",
+		"desc" : "Grants the ability to use piercing weapons."
+	},
+	{
+		"name" : "Large Weapons",
+		"desc" : "Grants the ability to use large weapons."
+	},
+	{
+		"name" : "Celtic Spear",
+		"desc" : "Grants the ability to use celtic spears."
+	},
+	{
+		"name" : "Scythe",
+		"desc" : "Grants the ability to use scythes."
 	}
 ];
 
 const ARMOR_ABILITY_DATA = [
-	
+	{
+		"name" : "Cloth",
+		"desc" : "Grants the ability to wear cloth armor."
+	},
+	{
+		"name" : "Leather",
+		"desc" : "Grants the ability to use leather armor."
+	},
+	{
+		"name" : "Studded",
+		"desc" : "Grants the ability to use studded armor."
+	},
+	{
+		"name" : "Chain",
+		"desc" : "Grants the ability to use chain armor."
+	},
+	{
+		"name" : "Plate",
+		"desc" : "Grants the ability to wear plate armor."
+	},
+	{
+		"name" : "Reinforced",
+		"desc" : "Grants the ability to wear reinforced armor."
+	},
+	{
+		"name" : "Scale",
+		"desc" : "Grants the ability to wear scale armor."
+	}
 ];
 
 const SHIELD_ABILITY_DATA = [
-	
+	{
+		"name" : "Small Shields",
+		"desc" : "Grants the ability to use small shields."
+	},
+	{
+		"name" : "Medium Shileds",
+		"desc" : "Grants the ability to use medium shields."
+	},
+	{
+		"name" : "Large Shields",
+		"desc" : "Grants the ability to use large shields."
+	}
 ];
 
