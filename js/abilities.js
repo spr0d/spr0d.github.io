0928946a86cae99ffa4cca874e7bb95d3a9cd470
Ryin Smith <ryin.smith@gmail.com>
2020-01-29 14:19:35 -0600

    Added comments to each ability for it's index in the array, and additional abilities/weapons from Midgard

--- js/abilities.js
+++ js/abilities.js
@@ -1,135 +1,167 @@
 const ABILITY_DATA = [
 	{
-		"name" : "Sprint",
+		"name" : "Sprint",// 0
 		"desc" : "Increases your movement speed by 30% of your base movement while your endurance lasts. Drains 5 endurance per second while active."
 	},
 	{
-		"name" : "Quick Cast",
+		"name" : "Quick Cast",// 1
 		"desc" : "Your next spell will not be interrupted. Your cast time will not be modified by any bonus including dexterity. Can be reused after 30 seconds."
 	}, 
 	{
-		"name" : "Evade I",
+		"name" : "Evade I",// 2
 		"desc" : "Increases your chance to evade by 5%."
 	},
 	{
-		"name" : "Evade II",
+		"name" : "Evade II",// 3
 		"desc" : "Increases your chance to evade by 10%."
 	},
 	{
-		"name" : "Tireless",
+		"name" : "Tireless",// 4
 		"desc" : "Increases endurance regeneration. Does not stack with other endurance regeneration spells or effects."
+	},
+	{
+		"name" : "Berserk I",// 5
+		"desc" : "You transform into a vendo and enter a berserker frenzy doing 10% more damage over 20 seconds."
+	},
+	{
+		"name" : "Protect I",// 6
+		"desc" : "You can use this on one grouped character. If they are within 1000 units of you and not currently engaged in melee combat with an enemy, 15% of their threat will be transferred to you."
 	}
 ];
 
 const WEAPONRY_DATA = [
 	{
-		"name" : "Staves",
+		"name" : "Staves",// 0 
 		"desc" : "Grants the ability to use staves."
 	},
 	{
-		"name" : "Polearms",
+		"name" : "Polearms",// 1
 		"desc" : "Grants the ability to use polearms."
 	},
 	{
-		"name" : "Two-Handed",
+		"name" : "Two-Handed",// 2
 		"desc" : "Grants the ability to use two handed weapons."
 	},
 	{
-		"name" : "Slashing",
+		"name" : "Slashing",// 3
 		"desc" : "Grants the ability to use slashing weapons."
 	},
 	{
-		"name" : "Crushing",
+		"name" : "Crushing",// 4
 		"desc" : "Grants the ability to use crushing weapons."
 	},
 	{
-		"name" : "Thrusting",
+		"name" : "Thrusting",// 5
 		"desc" : "Grants the ability to use thrusting weapons."
 	},
 	{
-		"name" : "Flexible",
+		"name" : "Flexible",// 6
 		"desc" : "Grants the ability to use flexible weapons."
 	},
 	{
-		"name" : "Crossbows",
+		"name" : "Crossbows",// 7
 		"desc" : "Grants the ability to use crossbows."
 	},
 	{
-		"name" : "Shortbows",
+		"name" : "Shortbows",// 8
 		"desc" : "Grants the ability to use shortbows."
 	},
 	{
-		"name" : "Archery",
+		"name" : "Archery",// 9
 		"desc" : "Grants the ability to use longbows."
 	},
 	{
-		"name" : "Blades",
+		"name" : "Blades",// 10
 		"desc" : "Grants the ability to use blades."
 	},
 	{
-		"name" : "Blunt",
+		"name" : "Blunt",// 11
 		"desc" : "Grants the ability to use blunt weapons."
 	},
 	{
-		"name" : "Piercing",
+		"name" : "Piercing",// 12
 		"desc" : "Grants the ability to use piercing weapons."
 	},
 	{
-		"name" : "Large Weapons",
+		"name" : "Large Weapons",// 13
 		"desc" : "Grants the ability to use large weapons."
 	},
 	{
-		"name" : "Celtic Spear",
+		"name" : "Celtic Spear",// 14
 		"desc" : "Grants the ability to use celtic spears."
 	},
 	{
-		"name" : "Scythe",
+		"name" : "Scythe",// 15
 		"desc" : "Grants the ability to use scythes."
+	},
+	{
+		"name" : "Swords",// 16
+		"desc" : "Grants the ability to use swords."
+	},
+	{
+		"name" : "Axes",// 17
+		"desc" : "Grants the ability to use axes."
+	},
+	{
+		"name" : "Hammers",// 18
+		"desc" : "Grants the ability to use hammers."
+	},
+	{
+		"name" : "Thrown",// 19
+		"desc" : "Grants thea bility to use thrown weapons."
+	},
+	{
+		"name" : "Left Axes",// 20
+		"desc" : "Grants the ability to use left axes."
+	},
+	{
+		"name" : "Hand to Hand",// 21
+		"desc" : "Grants the ability to use hand to hand weapons."
 	}
 ];
 
 const ARMOR_ABILITY_DATA = [
 	{
-		"name" : "Cloth",
+		"name" : "Cloth",// 0
 		"desc" : "Grants the ability to wear cloth armor."
 	},
 	{
-		"name" : "Leather",
+		"name" : "Leather",// 1
 		"desc" : "Grants the ability to use leather armor."
 	},
 	{
-		"name" : "Studded",
+		"name" : "Studded",// 2
 		"desc" : "Grants the ability to use studded armor."
 	},
 	{
-		"name" : "Chain",
+		"name" : "Chain",// 3
 		"desc" : "Grants the ability to use chain armor."
 	},
 	{
-		"name" : "Plate",
+		"name" : "Plate",// 4
 		"desc" : "Grants the ability to wear plate armor."
 	},
 	{
-		"name" : "Reinforced",
+		"name" : "Reinforced",// 5
 		"desc" : "Grants the ability to wear reinforced armor."
 	},
 	{
-		"name" : "Scale",
+		"name" : "Scale",// 6
 		"desc" : "Grants the ability to wear scale armor."
 	}
 ];
 
 const SHIELD_ABILITY_DATA = [
 	{
-		"name" : "Small Shields",
+		"name" : "Small Shields",// 0
 		"desc" : "Grants the ability to use small shields."
 	},
 	{
-		"name" : "Medium Shileds",
+		"name" : "Medium Shileds",// 1
 		"desc" : "Grants the ability to use medium shields."
 	},
 	{
-		"name" : "Large Shields",
+		"name" : "Large Shields",// 2
 		"desc" : "Grants the ability to use large shields."
 	}
 ];
